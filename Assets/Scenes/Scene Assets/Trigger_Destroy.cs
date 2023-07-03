66526f222840c693ee4e1ddd929c0928fbbc327a
Nico-Rivera <89566234+Nico-Rivera@users.noreply.github.com>
2023-07-02 22:47:34 -0400

    barrera 2

--- Assets/Scenes/Scene Assets/Trigger_Destroy.cs
+++ Assets/Scenes/Scene Assets/Trigger_Destroy.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Trigger_Destroy : MonoBehaviour
 {
-    public GameObject enemigo; 
-    private GameObject entity;
+    public GameObject enemigo1;
+    public GameObject enemigo2;
+    public GameObject enemigo3;
+    public GameObject enemigo4;
+
+    private GameObject entity1;
+    private GameObject entity2;
+    private GameObject entity3;
+    private GameObject entity4;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        entity = GameObject.Find(enemigo.name);
+        entity1 = GameObject.Find(enemigo1.name);
+        entity2 = GameObject.Find(enemigo2.name);
+        entity3 = GameObject.Find(enemigo4.name);
+        entity4 = GameObject.Find(enemigo4.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (entity == null) DestroyImmediate(gameObject);
+        if (entity1 == null && entity2 == null && entity3 == null && entity4 == null ) DestroyImmediate(gameObject);
     }
 }
